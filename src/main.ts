92d7af05b43046e7bb06a44171ed54b55ff10b3d
lebuckman <liambuckman0@gmail.com>
2026-04-25 23:34:20 -0700

    fix: disable devtools opening on start

--- src/main.ts
+++ src/main.ts
@@ -636,7 +636,7 @@ const createWindow = () => {
     );
   }
 
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
   mainWindow.on("closed", () => {
     mainWindow = null;
   });
